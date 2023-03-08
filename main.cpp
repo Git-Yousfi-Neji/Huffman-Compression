d0084546e285e1bbb254fb6383618bad964cba53
root <yousfineji1@yahoo.com>
2023-03-08 15:07:20 +0000

    update main

--- main.cpp
+++ main.cpp
@@ -8,19 +8,17 @@ int main(int argc, char* argv[]) {
     if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " [-c|-d] <filename>\n";
         return 1;
-    } 
+    }
     Huffman huffman;
     if (std::strcmp(argv[1], "-c") == 0) {
         std::cout<<argv[2]<<std::endl;
     // Encode the input file
         huffman.encode(argv[2], "encoded.bin");
-        
     }
     if (std::strcmp(argv[1], "-d") == 0) {
         std::cout<<argv[2]<<std::endl;
         // Decode the encoded file
         huffman.decode(argv[2], "decoded.txt");
-        
     }
     return 0;
 }
